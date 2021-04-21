0f7ceba57ca14e46e0fab2a1c756b012751faf40
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-04-21 15:31:33 +0300

    Create youtube.py

---
+++ youtube.py
@@ -0,0 +1,18 @@
+print("Hi, what is you're name?!!??!?!?!?!??!?!?!!?!??!?!1!???????")
+user_name = input()
+
+if user_name == "Klaountio":
+    print("Ante gamhsoy malaka!")
+
+elif user_name == "Technoblade":
+    print("Not even close baby")
+
+elif user_name == "Dream":
+    print("We all know the truth about Goggy")
+
+elif user_name == "Will":
+    print("You're new boyfriend is an asshole")
+
+elif user_name == "Tech With Tim":
+    print("Thank's for learning me things ;)")
+
